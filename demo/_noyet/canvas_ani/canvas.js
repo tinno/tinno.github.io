364ac83d46a60dfa91d902ab8fb8480f12f13756
Wan Ling Cui <tinno326@gmail.com>
2015-01-27 20:43:03 +0800

    flash 转canvas

---
+++ demo/_noyet/canvas_ani/canvas.js
@@ -0,0 +1,75 @@
+(function (lib, img, cjs) {
+
+var p; // shortcut to reference prototypes
+
+// library properties:
+lib.properties = {
+	width: canvas.width,
+	height: canvas.height,
+	fps: 24,
+	color: "#000000",
+	manifest: []
+};
+
+
+
+// symbols:
+
+
+
+(lib.补间4 = function() {
+	this.initialize();
+
+	// 图层 1
+	this.shape = new cjs.Shape();
+	this.shape.graphics.f().ls(["#FFFFFF","#000000"],[0,1],-39.7,0,39.8,0).ss(1,1,1).p("AGIAAQAACihzBzQhzBziiAAQihAAhzhzQhzhzAAiiQAAihBzhzQBzhzChAAQCiAABzBzQBzBzAAChg");
+
+	this.shape_1 = new cjs.Shape();
+	this.shape_1.graphics.f("#00725D").s().p("AkUEVQhzhzAAiiQAAihBzhzQBzhzChAAQCiAABzBzQBzBzAAChQAACihzBzQhzBziiAAQihAAhzhzg");
+
+	this.addChild(this.shape_1,this.shape);
+}).prototype = p = new cjs.Container();
+p.nominalBounds = new cjs.Rectangle(-40.2,-40.2,80.5,80.5);
+
+
+(lib.补间3 = function() {
+	this.initialize();
+
+	// 图层 1
+	this.shape = new cjs.Shape();
+	this.shape.graphics.f().ls(["#FFFFFF","#000000"],[0,1],-39.7,0,39.8,0).ss(1,1,1).p("AGIAAQAACihzBzQhzBziiAAQihAAhzhzQhzhzAAiiQAAihBzhzQBzhzChAAQCiAABzBzQBzBzAAChg");
+
+	this.shape_1 = new cjs.Shape();
+	this.shape_1.graphics.f("#00725D").s().p("AkUEVQhzhzAAiiQAAihBzhzQBzhzChAAQCiAABzBzQBzBzAAChQAACihzBzQhzBziiAAQihAAhzhzg");
+
+	this.addChild(this.shape_1,this.shape);
+}).prototype = p = new cjs.Container();
+p.nominalBounds = new cjs.Rectangle(-40.2,-40.2,80.5,80.5);
+
+
+// stage content:
+(lib.无标题1 = function(mode,startPosition,loop) {
+if (loop == null) { loop = false; }	this.initialize(mode,startPosition,loop,{});
+
+	// 图层 2
+	this.instance = new lib.补间4("synched",0);
+	this.instance.setTransform(374.3,364.2);
+	this.instance._off = true;
+
+	this.timeline.addTween(cjs.Tween.get(this.instance).wait(34).to({_off:false},0).wait(1).to({scaleY:1.01,skewY:-1.1,y:357.7,alpha:0.988},0).wait(1).to({scaleY:1.01,skewY:-2.3,y:351.3,alpha:0.976},0).wait(1).to({scaleY:1.02,skewY:-3.4,y:344.8,alpha:0.965},0).wait(1).to({scaleY:1.03,skewY:-4.6,y:338.3,alpha:0.953},0).wait(1).to({scaleY:1.04,skewY:-5.7,y:331.9,alpha:0.941},0).wait(1).to({scaleY:1.05,skewY:-6.9,y:325.4,alpha:0.929},0).wait(1).to({scaleY:1.05,skewY:-8,y:318.9,alpha:0.918},0).wait(1).to({scaleY:1.06,skewY:-9.2,y:312.5,alpha:0.906},0).wait(1).to({scaleY:1.07,skewY:-10.3,y:306,alpha:0.894},0).wait(1).to({scaleY:1.08,skewY:-11.5,y:299.6,alpha:0.882},0).wait(1).to({scaleY:1.08,skewY:-12.6,y:293.1,alpha:0.871},0).wait(1).to({scaleY:1.09,skewY:-13.8,y:286.6,alpha:0.859},0).wait(1).to({scaleY:1.1,skewY:-14.9,y:280.2,alpha:0.847},0).wait(1).to({scaleY:1.11,skewY:-16.1,y:273.7,alpha:0.835},0).wait(1).to({scaleY:1.11,skewY:-17.2,y:267.2,alpha:0.824},0).wait(1).to({scaleY:1.12,skewY:-18.4,y:260.8,alpha:0.812},0).wait(1).to({scaleY:1.13,skewY:-19.5,y:254.3,alpha:0.8},0).wait(1).to({scaleY:1.14,skewY:-20.6,y:247.8,alpha:0.788},0).wait(1).to({scaleY:1.14,skewY:-21.8,y:241.4,alpha:0.776},0).wait(1).to({scaleY:1.15,skewY:-22.9,y:234.9,alpha:0.765},0).wait(1).to({scaleY:1.16,skewY:-24.1,y:228.4,alpha:0.753},0).wait(1).to({scaleY:1.17,skewY:-25.2,y:222,alpha:0.741},0).wait(1).to({scaleY:1.18,skewY:-26.4,y:215.5,alpha:0.729},0).wait(1).to({scaleY:1.18,skewY:-27.5,y:209.1,alpha:0.718},0).wait(1).to({scaleY:1.19,skewY:-28.7,y:202.6,alpha:0.706},0).wait(1).to({scaleY:1.2,skewY:-29.8,y:196.1,alpha:0.694},0).wait(1).to({scaleY:1.21,skewY:-31,y:189.7,alpha:0.682},0).wait(1).to({scaleY:1.21,skewY:-32.1,y:183.2,alpha:0.671},0).wait(1).to({scaleY:1.22,skewY:-33.3,y:176.7,alpha:0.659},0).wait(1).to({scaleY:1.23,skewY:-34.4,y:170.3,alpha:0.647},0).wait(1).to({scaleY:1.24,skewY:-35.6,y:163.8,alpha:0.635},0).wait(1).to({scaleY:1.24,skewY:-36.7,y:157.3,alpha:0.624},0).wait(1).to({scaleY:1.25,skewY:-37.9,y:150.9,alpha:0.612},0).wait(1).to({scaleY:1.26,skewY:-39,y:144.4,alpha:0.6},0).wait(1).to({scaleX:0.99,scaleY:1.26,y:138,alpha:0.598},0).wait(1).to({scaleX:0.98,scaleY:1.27,y:131.6,alpha:0.595},0).wait(1).to({scaleX:0.97,scaleY:1.28,y:125.2,alpha:0.592},0).wait(1).to({scaleX:0.96,scaleY:1.28,y:118.8,alpha:0.59},0).wait(1).to({scaleX:0.94,scaleY:1.29,y:112.4,alpha:0.587},0).wait(1).to({scaleX:0.93,scaleY:1.29,y:106,alpha:0.585},0).wait(1).to({scaleX:0.92,scaleY:1.3,y:99.6,alpha:0.582},0).wait(1).to({scaleX:0.91,scaleY:1.31,y:93.2,alpha:0.58},0).wait(1).to({scaleX:0.9,scaleY:1.31,y:86.8,alpha:0.577},0).wait(1).to({scaleX:0.89,scaleY:1.32,y:80.4,alpha:0.575},0).wait(1).to({scaleX:0.88,scaleY:1.32,y:74,alpha:0.572},0).wait(1).to({scaleX:0.87,scaleY:1.33,y:67.6,alpha:0.57},0).wait(1).to({scaleX:0.86,scaleY:1.34,y:61.2,alpha:0.568},0).wait(1).to({scaleX:0.84,scaleY:1.34,y:54.7,alpha:0.565},0).wait(1).to({scaleX:0.83,scaleY:1.35,y:48.3,alpha:0.563},0).wait(1).to({scaleX:0.82,scaleY:1.35,y:41.9,alpha:0.56},0).wait(1).to({scaleX:0.81,scaleY:1.36,y:35.5,alpha:0.558},0).wait(1).to({scaleX:0.8,scaleY:1.37,y:29.1,alpha:0.555},0).wait(1).to({scaleX:0.79,scaleY:1.37,y:22.7,alpha:0.553},0).wait(1).to({scaleX:0.78,scaleY:1.38,y:16.3,alpha:0.55},0).wait(1).to({scaleX:0.77,scaleY:1.38,y:9.9,alpha:0.548},0).wait(1).to({scaleX:0.76,scaleY:1.39,y:3.5,alpha:0.545},0).wait(1).to({scaleX:0.74,scaleY:1.4,y:-2.9,alpha:0.543},0).wait(1).to({scaleX:0.73,scaleY:1.4,y:-9.3,alpha:0.54},0).wait(1).to({scaleX:0.72,scaleY:1.41,y:-15.7,alpha:0.538},0).wait(1).to({scaleX:0.71,scaleY:1.41,y:-22.1,alpha:0.535},0).wait(1).to({scaleX:0.7,scaleY:1.42,skewY:-39.1,y:-28.5,alpha:0.533},0).wait(1).to({scaleX:0.69,scaleY:1.43,y:-34.9,alpha:0.53},0).wait(1).to({scaleX:0.68,scaleY:1.43,y:-41.3,alpha:0.527},0).wait(1).to({scaleX:0.67,scaleY:1.44,y:-47.7,alpha:0.525},0).wait(1).to({scaleX:0.66,scaleY:1.44,y:-54.1,alpha:0.522},0).wait(1).to({scaleX:0.64,scaleY:1.45,y:-60.5,alpha:0.52},0).wait(1).to({scaleX:0.63,scaleY:1.45,y:-66.9,alpha:0.517},0).wait(1).to({scaleX:0.62,scaleY:1.46,y:-73.3,alpha:0.515},0).wait(1).to({scaleX:0.61,scaleY:1.47,y:-79.8,alpha:0.512},0).wait(1).to({scaleX:0.6,scaleY:1.47,y:-86.2,alpha:0.51},0).wait(1).to({scaleX:0.59,scaleY:1.48,y:-92.6,alpha:0.507},0).wait(1).to({scaleX:0.58,scaleY:1.48,y:-99,alpha:0.505},0).wait(1).to({scaleX:0.57,scaleY:1.49,y:-105.4,alpha:0.502},0).wait(1).to({scaleX:0.56,scaleY:1.5,y:-111.8,alpha:0.5},0).wait(1));
+
+	// 图层 1
+	this.instance_1 = new lib.补间3("synched",0);
+	this.instance_1.setTransform(144.3,364.2);
+
+	this.instance_2 = new lib.补间4("synched",0);
+	this.instance_2.setTransform(374.3,364.2);
+
+	this.timeline.addTween(cjs.Tween.get({}).to({state:[{t:this.instance_1}]}).to({state:[{t:this.instance_2}]},19).to({state:[]},15).to({state:[]},1).wait(74));
+	this.timeline.addTween(cjs.Tween.get(this.instance_1).to({_off:true,x:374.3},19).wait(90));
+
+}).prototype = p = new cjs.MovieClip();
+p.nominalBounds = new cjs.Rectangle(424,804,80.5,80.5);
+
+})(lib = lib||{}, images = images||{}, createjs = createjs||{});
+var lib, images, createjs;
